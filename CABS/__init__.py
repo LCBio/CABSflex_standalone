ee10f004b425c6ca826efc3a26dead12046b4e3a
Nithin <nithin_aneesh@aol.in>
2026-02-12 12:50:23 +0100

    Updated version to 3.0.1

--- CABS/__init__.py
+++ CABS/__init__.py
@@ -8,7 +8,7 @@ for protein folding, flexibility analysis, and protein-protein docking simulatio
 from pathlib import Path
 from typing import List
 
-__version__ = "3.0"
+__version__ = "3.0.1"
 __author__ = "Laboratory of Computational Biology"
 __email__ = "k.wroblewski7@uw.edu.pl"
 __license__ = "MIT"
